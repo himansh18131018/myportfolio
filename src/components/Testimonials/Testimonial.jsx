3e60db77cbfb6e873d4e86008450377e2c774491
htiwari5 <tiwari_himanshu5@optum.com>
2022-08-19 16:54:38 +0530

    updates

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jpg";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      name: "Jiten Vij (Manager at Optum)",
+      review:
+        "Thank you Himanshu for all the great work which you are doing. You pushed yourself to learn the application quickly and excel in it. You have great commitment and dedication to do all the work with perfection. Not only you delivered PH work so smoothly and nicely, you have also fixed some existing defect in the system which help business in smooth authorisation experience.",
+    },
+    {
+      img: profilePic2,
+      name: "Jiten Vij",
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+    },
+    {
+      img: profilePic3,
+      name: "Jiten Vij",
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+    },
+    {
+      img: profilePic4,
+      name: "Jiten Vij",
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex officiis molestiae quod tempora laudantium, cumque error a nisi placeat quae exercitationem, maiores reiciendis! Eaque dicta minima, iure maiores dolorum sed.",
+    },
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>People always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+        <div
+          className="blur t-blur1"
+          style={{ background: "var(--purple)" }}
+        ></div>
+        <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <div className="t-heading">
+                  <span>{client.name}</span>
+                </div>
+                <span>{client.review}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
